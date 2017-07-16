e4fd2b136d9b010de992cc1eeabf1f98b659ec12
yytoo <yaot0102@163.com>
2017-07-16 14:29:57 +0800

    it's a demo for hiberate_one2many

---
+++ hibernate-one2many/src/com/one2many/entity/Student.java
@@ -0,0 +1,29 @@
+package com.one2many.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	private int sid;
+	private String sname;
+	private String sex;
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
